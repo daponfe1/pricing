faaf0bbeee21a926f59ab9c4e51192c2b4c4d1a8
daponfe1 <108303930+daponfe1@users.noreply.github.com>
2024-04-22 20:16:07 +0200

    Update PricingDBAdapter.java

--- infrastructure/src/main/java/com/inditex/pricing/infrastructure/adapter/database/repository/PricingDBAdapter.java
+++ infrastructure/src/main/java/com/inditex/pricing/infrastructure/adapter/database/repository/PricingDBAdapter.java
@@ -3,12 +3,9 @@ package com.inditex.pricing.infrastructure.adapter.database.repository;
 import com.inditex.pricing.application.exception.ApplicationException;
 import com.inditex.pricing.application.port.out.repository.PricingRepository;
 import com.inditex.pricing.domain.entity.Pricing;
-import com.inditex.pricing.infrastructure.adapter.database.entity.PricingEntity;
 import com.inditex.pricing.infrastructure.adapter.database.mapper.PricingRepositoryMapper;
 import com.inditex.pricing.infrastructure.adapter.database.repository.h2.PricingH2Repository;
 import java.time.LocalDateTime;
-import java.util.Comparator;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -25,17 +22,17 @@ public class PricingDBAdapter implements PricingRepository {
 
   public Pricing getPricing(LocalDateTime applicationDate, String productId, String brandId) {
 
-    List<PricingEntity> pricings =
-        pricingH2Repository.findByApplicationDatesAndProductAndBrand(
-            applicationDate, productId, brandId);
-
-    if (pricings.isEmpty()) {
-      throw new ApplicationException(
-          HttpStatus.NOT_FOUND,
-          String.format(ERROR_PRICING_NOT_FOUND, applicationDate.toString(), productId, brandId));
-    }
-    PricingEntity pricing =
-        pricings.stream().max(Comparator.comparing(PricingEntity::getPriority)).get();
-    return pricingRepositoryMapper.map(pricing);
+    return pricingRepositoryMapper.map(
+        pricingH2Repository
+            .findByApplicationDatesAndProductAndBrand(applicationDate, productId, brandId)
+            .orElseThrow(
+                () ->
+                    new ApplicationException(
+                        HttpStatus.NOT_FOUND,
+                        String.format(
+                            ERROR_PRICING_NOT_FOUND,
+                            applicationDate.toString(),
+                            productId,
+                            brandId))));
   }
 }
