a36eb5383a0b7d4152ef156e2e172a36788dea26
daponfe1 <108303930+daponfe1@users.noreply.github.com>
2024-04-22 20:16:33 +0200

    Update PricingH2Repository.java

--- infrastructure/src/main/java/com/inditex/pricing/infrastructure/adapter/database/repository/h2/PricingH2Repository.java
+++ infrastructure/src/main/java/com/inditex/pricing/infrastructure/adapter/database/repository/h2/PricingH2Repository.java
@@ -2,7 +2,7 @@ package com.inditex.pricing.infrastructure.adapter.database.repository.h2;
 
 import com.inditex.pricing.infrastructure.adapter.database.entity.PricingEntity;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,11 +14,13 @@ public interface PricingH2Repository extends JpaRepository<PricingEntity, UUID> 
 
   @Query(
       """
-			select p from  PricingEntity p
-			where p.endDate >= :applicationDate and p.startDate <= :applicationDate
-			and p.product.id = :productId and p.brand.id = :brandId
-			""")
-  List<PricingEntity> findByApplicationDatesAndProductAndBrand(
+  			select p from  PricingEntity p
+  			where p.endDate >= :applicationDate and p.startDate <= :applicationDate
+  			and p.product.id = :productId and p.brand.id = :brandId
+  			order by p.priority desc
+  			limit 1
+  			""")
+  Optional<PricingEntity> findByApplicationDatesAndProductAndBrand(
       @Param("applicationDate") LocalDateTime applicationDate,
       @Param("productId") String productId,
       @Param("brandId") String brandId);
